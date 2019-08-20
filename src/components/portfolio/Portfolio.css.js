3ba8c4fc11c15d65dc2edd0e4cdad2fd50815105
Brian Be <bebrian458@gmail.com>
2019-08-19 19:40:14 -0700

    Portfolio description

--- src/components/portfolio/Portfolio.css.js
+++ src/components/portfolio/Portfolio.css.js
@@ -1,1 +1,23 @@
 import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  min-height: calc(100vh - 50px);
+  background: #fff;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0px;
+  min-height: 130px;
+  max-width: 400px;
+  text-align: center;
+`;
+
+export { Container, Header };
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import { Container, Header } from './Portfolio.css.js';
 
 export class Portfolio extends Component {
   render() {
-    return <div>This is the portfolio page</div>;
+    return (
+      <Container>
+        <Header>
+          <h1>My Portfolio</h1>
+          <p>
+            I focus mainly on producing web apps, but I have also contributed in
+            other types of projects. These include mobile apps and 2D/3D games.
+          </p>
+        </Header>
+      </Container>
+    );
   }
 }
 
